070a2880b865e9276c8ddac1c2df1cfc979ba90a
Sattwik Palai <sattwikpalai21@gmail.com>
2021-06-27 00:42:26 +0530

    Areas we serve Section Completed

--- scripts/app.js
+++ scripts/app.js
@@ -60,3 +60,38 @@ function isElementInViewport(el) {
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
     );
 }
+
+//SCROLL ANIMATE 2
+var scroll3 = window.requestAnimationFrame ||
+    function(callback3) { window.setTimeout(callback3, 1000 / 60) };
+var elementsToShow3 = document.querySelectorAll('.show-on-scroll3');
+
+function loop3() {
+
+    Array.prototype.forEach.call(elementsToShow3, function(element) {
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible3');
+        } else {
+            element.classList.remove('is-visible3');
+        }
+    });
+
+    scroll3(loop3);
+}
+loop3();
+
+function isElementInViewport(el) {
+    // special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect3 = el.getBoundingClientRect();
+    return (
+        (rect3.top <= 0 &&
+            rect3.bottom >= 0) ||
+        (rect3.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect3.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+        (rect3.top >= 0 &&
+            rect3.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+}
