bda3f06e00c60ab07dffc9cdecea56df6e53845a
sudiptapradhan <sudiptap141@gmail.com>
2021-06-27 14:59:28 +0530

    our services section completed

--- scripts/app.js
+++ scripts/app.js
@@ -26,6 +26,29 @@ function opennav() {
     }
 }
 
+
+//TESTIMONIAL
+$(document).ready(function(){
+        pagenum = 1;
+        function AutoRotate() {
+           var myele = null;
+           var allElements = document.getElementsByTagName('label');
+           for (var i = 0, n = allElements.length; i < n; i++) {
+               var myfor = allElements[i].getAttribute('for');
+               if ((myfor !== null) && (myfor == ('slide_2_' + pagenum))) {
+                   allElements[i].click();
+                   break;
+               }
+           }
+           if (pagenum == 4) {
+               pagenum = 1;
+           } else {
+               pagenum++;
+           }
+        }
+        setInterval(AutoRotate, 5000);
+    });
+
 //SCROLL ANIMATE
 var scroll = window.requestAnimationFrame ||
     function(callback) { window.setTimeout(callback, 1000 / 60) };
